b37f24f7d3d567c45081a07b76e1a4b81cef7f21
Shamsear <4pa20cs104@pace.edu.in>
2025-04-08 16:18:39 +0530

    ss

--- app.py
+++ app.py
@@ -309,7 +309,5 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True) 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
